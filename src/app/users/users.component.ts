aa6de3e36b20b265a896dc7474a022c2020f3481
reamplify <126955026+reamplify1@users.noreply.github.com>
2026-03-04 20:43:58 +0500

    users component changed

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-users',
@@ -8,28 +7,5 @@ import { LocalStorageService } from '../services/local-storage.service';
   styleUrl: './users.component.scss',
 })
 export class UsersComponent {
-
-    localStorageService: LocalStorageService = inject(LocalStorageService);
-
-  constructor() {
-    this.saveLastVisit();
-    this.saveVisitCount();
-  }
-
-    saveLastVisit(): void {
-    const LAST_VISIT_KEY: string = 'last-visit';
-    const now: Date = new Date();
-    this.localStorageService.setItem(LAST_VISIT_KEY, now.toString());
-  }
-
-  saveVisitCount(): void {
-    const VISIT_COUNT_KEY: string = 'visit-count';
-    const visits: string | null = this.localStorageService.getItem(VISIT_COUNT_KEY);
-
-    let count: number = visits ? parseInt(visits) : 0;
-    count++;
-
-    this.localStorageService.setItem(VISIT_COUNT_KEY, count.toString());
-  }
 
 }
