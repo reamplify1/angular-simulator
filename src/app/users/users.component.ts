0308839135c090ecd37baf7d2a9f1484c57ff8ea
reamplify <126955026+reamplify1@users.noreply.github.com>
2026-04-10 12:55:00 +0500

    feature/homework-20

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,17 +2,21 @@ import { Component, inject } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AsyncPipe } from '@angular/common';
 import { Observable, tap } from 'rxjs';
-import { IUser } from '../interfaces/IUser';
+import type { IUser } from '../interfaces/IUser';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { NotificationService } from '../services/notification.service';
+import { CreateUserComponent } from '../create-user/create-user.component';
 
 @Component({
   selector: 'app-users',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, UserCardComponent, CreateUserComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
 export class UsersComponent {
-
+  private notificationService: NotificationService = inject(NotificationService)
   private userService: UserService = inject(UserService);
+  loading: boolean = false;
 
   users$: Observable<IUser[]> = this.userService.users$;
 
@@ -22,7 +26,33 @@ export class UsersComponent {
         tap((users: IUser[]) => {
           this.userService.setUsers(users);
         })
-    ).subscribe();
+    )
+    .subscribe();
+  }
+
+  deleteUser(id: number): void {
+    const users: IUser[] = this.userService.getUsers().filter(user => user.id !== id);
+    this.userService.setUsers(users);
+  }
+
+  refresh(): void {
+    this.loading = true;
+    this.userService.loadUsers(true)
+    .pipe(
+      tap((users: IUser[]) =>
+        this.userService.setUsers(users))
+    )
+    .subscribe({
+      next: (users: IUser[]): void => {
+        this.notificationService.showSuccess('Users are updated');
+      },
+      complete: (): void => {
+      this.loading = false;
+      },
+      error: (): void => {
+        this.loading = false;
+      }
+    });
   }
 
 }
