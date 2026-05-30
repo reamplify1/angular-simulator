ddcef3776e0bfad4548abf23ceb0eb25a96fd418
reamplify <126955026+reamplify1@users.noreply.github.com>
2026-05-30 18:10:11 +0500

    conflict resolved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,20 @@ import { UsersComponent } from './users/users.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'users', component: UsersComponent },
-  { path: '**', component: NotFoundPageComponent },
+  {
+    path: '',
+    loadComponent: () => import('./home-page/home-page.component').then(m => m.HomePageComponent)
+  },
+  {
+    path: 'users',
+    loadComponent: () => import('./users/users.component').then(m => m.UsersComponent)
+  },
+  {
+    path: 'posts',
+    loadComponent: () => import('./features/posts/posts.component').then(m => m.PostsComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./not-found-page/not-found-page.component').then(m => m.NotFoundPageComponent)
+  },
 ];
